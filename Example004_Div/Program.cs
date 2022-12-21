02d962a9225a272b728845367d27113325327f7a
evgenia <chibichanga@yandex.ru>
2022-12-21 19:00:14 +0700

    add gitignore

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double numberB = 5;
+double numberA = 12;
+Console.WriteLine(numberA / numberB);
+
