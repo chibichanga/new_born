8fd160cdd4430665d6fd83bbe4eac414e87fc409
evgenia <chibichanga@yandex.ru>
2022-12-22 15:39:07 +0700

    практика не семинаре

---
+++ Seminar_1/Program.cs
@@ -0,0 +1,9 @@
+﻿// напишите программу, которая на вход принимает число
+// и выдает его квадрат (число умноженное на само себя)
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32 (Console.ReadLine());
+
+int sqt = number*number;
+
+Console.WriteLine(sqt);
---
+++ Seminar_1_1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишете программу, которая на выход принмает 
+// два числа и проверяет, является ли первое число квадротом второго
+// а = 25, b = 5 -> да
+// а = 2, b = 10 -> нет
+// а = 9, b = -3 -> да
+// а = -3, b = 9 -> нет
+
+Console.WriteLine("Введите число");
+int number1 = Convert.ToInt32 (Console.ReadLine());
+
+Console.WriteLine("Введите число");
+int number2 = Convert.ToInt32 (Console.ReadLine());
+
+if(number1 == number2*number2)
+{
+    Console.WriteLine("Да");
+}
+
+else
+{
+    Console.WriteLine("нет");
+}
+
+
+
+
+
