4325ef9a9c514f8f7c589a792b5c0b28f7441365
evgenia <chibichanga@yandex.ru>
2022-12-22 16:13:30 +0700

    практика обновление

---
+++ Seminar_1_4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принмает одно число (N), а на выходе показывает все целые числа 
+// в промежутке от -N до N.
+// 4 -> "-4 -3 -2 -1 0 1 2 3 4"
+// 2 -> "-2 -1 0 1 2"
+
+Console.WriteLine("Введите число");
+int number1 = Convert.ToInt32 (Console.ReadLine());
+
+int numberNeg = -number1;
+
+while(numberNeg <= number1)
+{
+    Console.WriteLine(numberNeg);
+    numberNeg = numberNeg + 1;
+}
---
+++ Seminar_1_5/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число и на выходе показывает
+// последнюю цифру этого числа. 456 -> 6 782 -> 2 918 ->8
+
+Console.WriteLine("Введите число");
+int number1 = Convert.ToInt32 (Console.ReadLine());
+
+int result = number1%10;
+
+Console.WriteLine(result);
+
+
