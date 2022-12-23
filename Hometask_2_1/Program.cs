63e44e78888964835e1fda132a2723fceb89a69f
evgenia <chibichanga@yandex.ru>
2022-12-23 17:21:15 +0700

    Д/з 2.1 вторая цифра из трехзначного числа

---
+++ Hometask_2_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает 
+// вторую цифру этого числа.
+// 456 -> 5, 782 -> 8, 918 -> 1
+
+int num = new Random().Next(100,1000);
+
+Console.WriteLine(num);
+
+int num1 = num%100;
+int num2 = num1/10;
+
+
+int result = num2;
+Console.WriteLine(result);
