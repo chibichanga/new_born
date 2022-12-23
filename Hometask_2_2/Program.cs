f24db4aa5b7e93596e1b819623714376899f4ec4
evgenia <chibichanga@yandex.ru>
2022-12-23 17:32:00 +0700

    Д/з 2.2 вывод третей цифры из заданого числа

---
+++ Hometask_2_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int num = new Random().Next(10,1000);
+Console.WriteLine(num);
+
+int result = num/100;
+
+if (result == 0)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+else
+{
+Console.WriteLine(result);
+}
