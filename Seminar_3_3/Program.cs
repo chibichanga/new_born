27bfb2648e6f155136d603c2c1d0715322d1ebcb
evgenia <chibichanga@yandex.ru>
2022-12-24 16:20:07 +0700

    нахождение квадрата чисел до N

---
+++ Seminar_3_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// напишите программу, которая принимает на вход число (N) и выдает таблицу квадратов 
+// чисел от 1 до N/
+// 5 - 1, 4, 9, 16, 25; 2- 1,4
+
+Console.WriteLine ("введите число N");
+int num1 = Convert.ToInt32 (Console.ReadLine());
+int num2 = 1;
+
+while (num2 <= num1)
+{
+    Console.WriteLine (num2*num2);
+    num2 = num2 + 1;
+}
+
