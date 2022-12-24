da8c48def1bc4ca5a5cb3fabf747942b5df4453e
evgenia <chibichanga@yandex.ru>
2022-12-24 15:45:48 +0700

    нахождение точки по координатам x,y

---
+++ Seminar_3_1/Program.cs
@@ -0,0 +1,26 @@
+﻿// напишите программу, которая принимает на вход координаты точки (x и y), причем
+// x не равно 0, и y не равно 0 и выдает номер четверти проскости, в которой находится эта точка.
+
+Console.WriteLine ("Координата X");
+int x = Convert.ToInt32 (Console.ReadLine());
+
+Console.WriteLine ("Координата Y");
+int y = Convert.ToInt32 (Console.ReadLine());
+
+if ((x > 0) && (y > 0))
+{
+    Console.WriteLine ("Первая четверть");
+}
+else if ((x < 0) && (y > 0))
+{
+    Console.WriteLine ("Вторая четверть");
+}
+else if ((x < 0) && (y < 0))
+{
+    Console.WriteLine ("Третья четверть");
+}
+// можно внутренние скобки не ставить.
+else if (x > 0 && y < 0)
+{
+    Console.WriteLine ("Четвертая четверть");
+}
